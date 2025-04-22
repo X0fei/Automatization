0e0bdd1a77bb16fab248f0158176c98a885f7ec2
X0fei <pitonvano05@gmail.com>
2025-04-22 09:53:42 +0300

    Добавьте файлы проекта.

---
+++ Automatization/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Automatization
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
