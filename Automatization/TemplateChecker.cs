1cab8198e939e2849e2474b76bf4db157631d5be
X0fei <pitonvano05@gmail.com>
2025-04-22 14:14:02 +0300

    warning start

---
+++ Automatization/TemplateChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+
+public static class TemplateChecker
+{
+    public static bool IsTemplateValid(string templatePath)
+    {
+        return File.Exists(templatePath) && Path.GetExtension(templatePath).ToLower() == ".docx";
+    }
+}
